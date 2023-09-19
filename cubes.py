760a1cad9b39f3b38af6f31e9c761f39517b2ef6
SsenkingaAbdul <abdulssenkinga21@gmail.com>
2023-09-19 12:13:37 +0300

    cubes of 1 - 9

---
+++ cubes.py
@@ -0,0 +1,5 @@
+from function1 import cube
+
+for i in range(10):
+    print(f"The cube of {i} is {cube(i)}.")
+
--- function1.py
+++ function1.py
@@ -1,6 +1,5 @@
 def square(x):
     return (x * x)
 
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")
-
+def cube(y):
+    return (y * y * y)
