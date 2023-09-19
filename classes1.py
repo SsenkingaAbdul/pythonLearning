783fbb3a3811076e0728ebcfd92d7a03a0224ff8
SsenkingaAbdul <abdulssenkinga21@gmail.com>
2023-09-19 13:28:08 +0300

    Point class

---
+++ classes1.py
@@ -0,0 +1,7 @@
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+    
+p = Point(2, 5)
+print(f"The x coordinate of the point is {p.x}, and that of the y coordinate is  {p.y}")
